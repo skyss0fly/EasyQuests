dae052c940ce2ecdecfdf860882314176de56ccc
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 15:01:18 +1000

    Create Main.php

---
+++ src/skyss0fly/EasyQuests/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace skyss0fly\EasyQuests;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use skyss0fly\EasyQuests\{SimpleForm, Form};
+use pocketmine\event\Listener;
+
+class main extends pluginBase implements Listener {
+
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool;
+
+}
