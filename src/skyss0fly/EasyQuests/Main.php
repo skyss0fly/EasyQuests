f177ece1c3ab16f214bc1d50899053aed45bb807
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 15:10:07 +1000

    Update Main.php

--- src/skyss0fly/EasyQuests/Main.php
+++ src/skyss0fly/EasyQuests/Main.php
@@ -12,6 +12,39 @@ class main extends pluginBase implements Listener {
 public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
 switch ($command->getName()) {
   case "quests":
+  
+         
+        $questsform = new SimpleForm(function (Player $player, $data){
+            $result = $data;
+            if ($sender->hasPermission("EasyQuests.Admin") {
+        $admun = $questsform->addButton("§c§lAdmin");
+          
+            if ($result !== null) {
+        
+                switch ($result) {
+                    case 0:
+                  
+                  $sender->sendMessage("Closed Form");
+                    break;
+                }
+            }
+        if ($admin)
+        {
+$this->admin();
+        }
+        });
+        $formtitle = "Quests"
+        $formcontent = $this->quests($content);
+    
+       
+        $questsform->setTitle($formtitle);
+        $questsform->setContent($formcontent);
+
+}
+  $questsform->addButton("§d§lSubmit");
+        $questsform->sendForm($questsform);
+  return true;
+    }
               
 }
 }
